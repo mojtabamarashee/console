0f3a5505a6dd1c2686cce10f109e0958012f4644
Mojtaba <mojtabamarashee@gmail.com>
2020-06-21 20:13:40 +0430

    test

--- console.js
+++ console.js
@@ -37,12 +37,14 @@ function every859am(yourcode) {
 }
 
 var sidebar;
-
+$('body').css({
+	'padding-right': '350px',
+});
 sidebar = $(`
-<div  id="sidebar" style="padding:10px">
+<div dir="ltr" id="sidebar" style="padding:10px">
 	<span style="float:left; color:black">fetch</span>
 	<input
-		style="float:right; color:black; "
+		style="float:right; color:black; width:100%"
 		type="text"
 		id="fetch-input"
 		oninput="{console.log(this.value)}"
@@ -54,68 +56,94 @@ sidebar = $(`
 	<br />
 	<form>
 		<div class="form-row">
-			<div class="form-group col-md-3">
-				<label style="float:left" class="text-dark" for="hour-input>hour</label>
+			<div class="form-group col-md-6">
+				<label style="float:left" class="text-dark" for="hour-input">
+					hour
+				</label>
 				<input
 					style="color:black"
 					type="text"
 					id="hour-input"
 					oninput="{console.log(this.value)}"
 					placeholder="hour"
+					class="form-control"
 				/>
 			</div>
-			<div class="form-group col-md-3">
-				<label style="float:left" class="text-dark" for="minute">min</label>
+			<div class="form-group col-md-6">
+				<label style="float:left" class="text-dark" for="minute-input">
+					min
+				</label>
 				<input
 					style="color:black"
 					type="text"
-					id="minute"
+					id="minute-input"
 					oninput="{console.log(this.value)}"
 					placeholder="minute"
+					class="form-control"
 				/>
 			</div>
-		
-		
-			<div class="form-group col-md-3">
-				<label style="float:left" class="text-dark" for="sec-input">sec</label>
+		</div>
+		<div class="form-row">
+			<div class="form-group col-md-6">
+				<label style="float:left" class="text-dark" for="sec-input">
+					sec
+				</label>
 				<input
 					style="color:black"
 					type="text"
 					id="sec-input"
 					oninput="{console.log(this.value)}"
 					placeholder="sec"
+					class="form-control"
 				/>
 			</div>
-			<div class="form-group col-md-3">
-				<label style="float:left" class="text-dark" for="milli-sec-input">millisec</label>
+			<div class="form-group col-md-6">
+				<label style="float:left" class="text-dark" for="milli-sec-input">
+					millisec
+				</label>
 				<input
 					style="color:black"
 					type="text"
 					id="milli-sec-input"
 					oninput="{console.log(this.value)}"
-					placeholder="millisec"
+					placeholder="milli sec"
+					class="form-control"
+				/>
+			</div>
+		</div>
+		<div class="form-row">
+			<div class="form-group col-md-6">
+				<label style="float:left" class="text-dark" for="delay-input">
+					delay
+				</label>
+				<input
+					style="color:black"
+					type="text"
+					id="delay-input"
+					oninput="{console.log(this.value)}"
+					placeholder="delay"
+					class="form-control"
+				/>
+			</div>
+			<div class="form-group col-md-6">
+				<label style="float:left" class="text-dark" for="interval-input">
+					interval
+				</label>
+				<input
+					style="color:black"
+					type="text"
+					id="interval-input"
+					oninput="{console.log(this.value)}"
+					placeholder="interval"
+					class="form-control"
 				/>
 			</div>
 		</div>
 	</form>
 	<br />
 	<br />
-	<span style="float:left; color:black">interval</span>
-	<input
-		style="float:right; color:black"
-		type="text"
-		id="interval-input"
-		oninput="{console.log(this.value)}"
-	/>
 	<br />
 	<br />
-	<span style="float:left; color:black">delay</span>
-	<input
-		style="float:right; color:black"
-		type="text"
-		id="delay-input"
-		oninput="{console.log(this.value)}"
-	/>
 	<br />
 	<br />
 	<br />
@@ -130,24 +158,17 @@ sidebar = $(`
 		class="btn btn-primary"
 		style="float:center; width:90%">
 		Start
-			
 	</button>
-	<br />
-	<br />
-	<br />
-	<br />
-	<br />
 </div>;
-
   `);
 sidebar.css({
 	position: 'fixed',
 	right: '0px',
 	top: '0px',
 	'z-index': 9999,
-	width: '350px',
+	width: '290px',
+    height:'100%',
 	'overflow-y': 'scroll',
 	'background-color': 'red', // Confirm it shows up
-	'max-height': '100%',
 });
 $('body').append(sidebar);
