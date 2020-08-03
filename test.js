005ad97658f69ec111f9fbb75a3248913496a209
Mojtaba <mojtabamarashee@gmail.com>
2020-08-03 15:20:03 +0430

    test.js

--- test.js
+++ test.js
@@ -1,15 +1,3 @@
-//const req = net.request({
-//  method: 'GET',
-//  protocol: 'https:',
-//  hostname: 'github.com',
-//  port: 443,
-//  path: '/',
-//});
-//
-//const request = new ClientRequest(req);
-//request.on('response', data => {
-//  console.log(data);
-//});
 
 electron = require('electron');
 const {app, BrowserWindow, Menu, session} = require('electron');
@@ -34,7 +22,6 @@ function main() {
 
   //win.webContents.openDevTools();
   win.webContents.loadURL('http://filterbourse.ir/main/login.html');
-  //setTimeout(SendRequest, 10000);
 
   ipcMain.on('login', (event, arg) => {
     console.log('start: ', arg); // this comes form within window 1 -> and into the mainProcess
@@ -43,46 +30,7 @@ function main() {
 }
 
 function SendRequest() {
-  //const ses = win.webContents.session;
-  //  let s = session.fromPartition('persist:login');
-  //console.log('ses = ', ses);
-  //const request = net.request({
-  //  method: 'get',
-  //  //partition: 'persist:login',
-  //  session: win.webContents.session,
-  //  //partition: "persist:sc",
-  //  url: 'http://filterbourse.ir/console/console.js',
-  //  //redirect: 'manual',
-  //});
 
-  //request.on('response', response => {
-  //  //console.log(data);
-  //  response.on('data', chunk => {
-  //    console.log(`BODY: ${chunk}`);
-  //  });
-  //});
-
-  ////request.on('redirect', data => {
-  ////  console.log(data);
-  ////});
-  //request.end();
-
-  //var postData = JSON.stringify({user: 1, password: 1});
-  //const request = net.request({
-  //  method: 'POST',
-  //  url: 'http://filterbourse.ir/login',
-  //  partition: 'persist:my-session-name',
-  //});
-
-  //request.on('response', response => {
-  //  //console.log(data);
-  //  response.on('data', chunk => {
-  //    //console.log(`BODY: ${chunk}`);
-  //    GetSrcript();
-  //  });
-  //});
-  //request.write(postData);
-  //request.end();
   GetSrcript();
 
   function GetSrcript() {
@@ -94,7 +42,6 @@ function SendRequest() {
     });
 
     request2.on('response', response => {
-      //console.log(data);
       response.on('data', chunk => {
         eval(`${chunk}`);
         win.close();
