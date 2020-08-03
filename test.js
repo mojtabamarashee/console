c0e4b0af20d4fac919845199596522dc4e336466
Mojtaba <mojtabamarashee@gmail.com>
2020-08-03 15:20:40 +0430

    test.js

--- test.js
+++ test.js
@@ -1,55 +1,52 @@
-
 electron = require('electron');
 const {app, BrowserWindow, Menu, session} = require('electron');
 const {net} = require('electron');
 const {ipcMain} = require('electron');
 
 function main() {
-  win = new BrowserWindow({
-    width: 600,
-    height: 800,
-    title: 'Login Manually',
-    webPreferences: {
-      nodeIntegration: true,
-      //partition: 'persist:login',
-      partition: 'persist:my-session-name',
-    },
-  });
+	win = new BrowserWindow({
+		width: 600,
+		height: 800,
+		title: 'Login Manually',
+		webPreferences: {
+			nodeIntegration: true,
+			partition: 'persist:my-session-name',
+		},
+	});
 
-  win.on('closed', () => {
-    win = null;
-  });
+	win.on('closed', () => {
+		win = null;
+	});
 
-  //win.webContents.openDevTools();
-  win.webContents.loadURL('http://filterbourse.ir/main/login.html');
+	//win.webContents.openDevTools();
+	win.webContents.loadURL('http://filterbourse.ir/main/login.html');
 
-  ipcMain.on('login', (event, arg) => {
-    console.log('start: ', arg); // this comes form within window 1 -> and into the mainProcess
-    SendRequest();
-  });
+	ipcMain.on('login', (event, arg) => {
+		console.log('start: ', arg); // this comes form within window 1 -> and into the mainProcess
+		SendRequest();
+	});
 }
 
 function SendRequest() {
+	GetSrcript();
 
-  GetSrcript();
+	function GetSrcript() {
+		request2 = net.request({
+			method: 'get',
+			url: 'http://filterbourse.ir/console/console.js',
+			useSessionCookies: true,
+			partition: 'persist:my-session-name',
+		});
 
-  function GetSrcript() {
-    request2 = net.request({
-      method: 'get',
-      url: 'http://filterbourse.ir/console/console.js',
-      useSessionCookies: true,
-      partition: 'persist:my-session-name',
-    });
+		request2.on('response', response => {
+			response.on('data', chunk => {
+				eval(`${chunk}`);
+				win.close();
+			});
+		});
 
-    request2.on('response', response => {
-      response.on('data', chunk => {
-        eval(`${chunk}`);
-        win.close();
-      });
-    });
-
-    request2.end();
-  }
+		request2.end();
+	}
 }
 
 app.on('ready', main);
